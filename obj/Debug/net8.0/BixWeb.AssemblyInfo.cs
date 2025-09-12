75c2822b9f459d8faa97bc0b97a5659a730cf5a4
Pedro Lucas <pedrolucasmota2005@gmail.com>
2025-09-12 16:55:31 -0300

    pequenas mudanças

--- obj/Debug/net8.0/BixWeb.AssemblyInfo.cs
+++ obj/Debug/net8.0/BixWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BixWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3f81ce0db78b6f17241d2af209b5e5df84c9178")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dd6170d50677d1358655380fd9ebb24436e2298")]
 [assembly: System.Reflection.AssemblyProductAttribute("BixWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BixWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
